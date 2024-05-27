2e21d5dcce26da64528865b2664e7f02d57ee4cd
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-05-27 11:48:15 +0300

    feat: implement calculator logic, button structure
    Add buttons in a grid layout. The backspace and clear buttons can be
    used to further manipulate the numbers in the display.
    
    This approach utilises a finite state machine, where the 5 types of
    input (clear, backspace, digit, operator, equals) are used to
    transition between 4 states (FIRST, OPERATOR, SECOND, RESULT).
    
    The current stage is shown for debug purposes.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+const displayText = document.querySelector(".display-text");
+const stageText = document.querySelector(".stage-text");
+const buttons = document.querySelectorAll("button");
+
 function add(a, b) {
   return a + b;
 }
@@ -16,7 +20,9 @@ function divide(a, b) {
 
 let first = 0;
 let second = 0;
+let result = 0;
 let operator = '';
+let currentStage = "FIRST"; // "FIRST" | "OPERATOR" | "SECOND" | "RESULT"
 
 function operate(a, op, b) {
   switch(op) {
@@ -29,3 +35,144 @@ function operate(a, op, b) {
   }
   return divide(a, b);
 }
+
+function setStage(stage) {
+  currentStage = stage;
+  stageText.textContent = stage;
+}
+
+function backspace() {
+  let currentText = displayText.textContent;
+  if(currentText.length > 2 || (currentText.length === 2 && currentText.charAt(0) !== '-')) {
+    displayText.textContent = currentText.slice(0, currentText.length - 1);
+  } else {
+    displayText.textContent = '0';
+  }
+}
+
+function clearDisplay() {
+  displayText.textContent = '0';
+}
+
+function setDisplay(val) {
+  displayText.textContent = val;
+} 
+
+function addDigit(digit) {
+  let currentText = displayText.textContent;
+  if(digit === '.') {
+    if(currentText.slice(-1) >= '0' && currentText.slice(-1) <= '9' && !currentText.includes('.')) {
+      displayText.textContent += '.';
+    } 
+  } else {
+    if(currentText === '0' || currentText === '') {
+      displayText.textContent = digit;
+    } else {
+      displayText.textContent += digit;
+    }
+  }
+}
+
+function addOperator(val) {
+  operator = val;
+  if(val === '*') {
+    displayText.textContent = '×';
+  } else if(val === '/') {
+    displayText.textContent = '÷';
+  } else displayText.textContent = val;
+}
+
+function calculate(a, op, b) {
+  let x = operate(a, op, b);
+  displayText.textContent = `${x}`;
+  return x;
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(button.classList.contains("back")) {
+      switch(currentStage) {
+        case "FIRST":
+        case "SECOND":
+          backspace();
+          break;
+        case "OPERATOR":
+          break;
+        case "RESULT":
+          setStage("FIRST");
+          backspace();
+          break;
+      }
+    } else if(button.classList.contains("clear")) {
+      switch(currentStage) {
+        case "FIRST":
+        case "SECOND":
+          clearDisplay();
+          break;
+        case "OPERATOR":
+        case "RESULT":
+          setStage("FIRST");
+          clearDisplay();
+          break;
+      }
+    } else if(button.classList.contains("digit")) {
+      switch(currentStage) {
+        case "FIRST":
+          addDigit(button.id);
+          break;
+        case "OPERATOR":
+          setStage("SECOND");
+          clearDisplay();
+          addDigit(button.id);
+          break;
+        case "SECOND":
+          addDigit(button.id);
+          break;
+        case "RESULT":
+          setStage("FIRST");
+          clearDisplay();
+          addDigit(button.id);
+          break;
+      }
+    } else if(button.classList.contains("operator")) {
+      switch(currentStage) {
+        case "FIRST":
+          setStage("OPERATOR");
+          first = +displayText.textContent;
+          addOperator(button.id);
+          break;
+        case "OPERATOR":
+          clearDisplay();
+          addOperator(button.id);
+          break;
+        case "SECOND":
+          setStage("OPERATOR");
+          second = +displayText.textContent;
+          first = calculate(first, operator, second);
+          addOperator(button.id);
+          setDisplay(first);
+          break;
+        case "RESULT":
+          setStage("OPERATOR");
+          first = result;
+          addOperator(button.id);
+          break;
+      }
+    } else if(button.classList.contains("equals")) {
+      switch(currentStage) {
+        case "FIRST":
+        case "OPERATOR":
+          break;
+        case "SECOND":
+          setStage("RESULT");
+          second = +displayText.textContent;
+          result = calculate(first, operator, second);
+          break;
+        case "RESULT":
+          result = calculate(result, operator, second);
+          break;
+      }
+    }
+  });
+});
